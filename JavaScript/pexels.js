f57a052300f9b57b8e923cef7a4a7a0fd8aea12a
AngeloZorzi <astro.drg666@gmail.com>
2025-03-20 16:20:34 +0100

    Update pexels.js

--- JavaScript/pexels.js
+++ JavaScript/pexels.js
@@ -22,6 +22,11 @@ const getImages = function () {
         if (photos[index]) {
           img.src = photos[index].src.medium;
           img.alt = photos[index].alt;
+          const card = img.closest(".card");
+
+          const smallText = card.querySelector("small.text-muted");
+
+          smallText.textContent = photos[index].id;
         }
       });
     })
@@ -34,3 +39,62 @@ document.getElementById("loadImages").addEventListener("click", function (e) {
   e.preventDefault();
   getImages();
 });
+
+const getImages1 = function () {
+  const imagesUrl = "https://api.pexels.com/v1/search?query=kittens";
+  fetch(imagesUrl, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "X7At0l5INFIQWKUBpQf4o57vfCgjeffD7KHo9hhBePsD4xivylf6futo",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("La risposta non è valida");
+      }
+    })
+    .then((data) => {
+      console.log("DATA:", data);
+      const images = document.querySelectorAll(".loadImg");
+      const photos = data.photos;
+
+      images.forEach((img, index) => {
+        if (photos[index]) {
+          img.src = photos[index].src.medium;
+          img.alt = photos[index].alt;
+          const card = img.closest(".card");
+
+          const smallText = card.querySelector("small.text-muted");
+
+          smallText.textContent = photos[index].id;
+        }
+      });
+    })
+    .catch((err) => {
+      console.log("Errore:", err);
+    });
+};
+
+document.getElementById("loadImages2").addEventListener("click", function (e) {
+  e.preventDefault();
+  getImages1();
+});
+
+const hideButtons = () => {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    const btnGroup = card.querySelector(".btn-group");
+    const editBtn = btnGroup.querySelector("button:nth-child(2)");
+
+    editBtn.textContent = "Hide";
+
+    editBtn.addEventListener("click", () => {
+      card.remove();
+    });
+  });
+};
+
+hideButtons();
